f3c42d2afe7d6e613789e32b18b2216478be82d3
J. Paul Daigle <j.paul.daigle@gmail.com>
2014-11-27 07:38:58 -0500

    Update config.php
    port number

--- modules/bitpay/config.php
+++ modules/bitpay/config.php
@@ -3,7 +3,7 @@
   $bitpayurl = "https://bitpay.com";
   //$bitpayurl = "https://test.bitpay.com";
   $apiurl = $bitpayurl;
-  $sslport = 8088;
+  $sslport = 443;
   $verifypeer = 1;
   $verifyhost = 2;
 ?>
